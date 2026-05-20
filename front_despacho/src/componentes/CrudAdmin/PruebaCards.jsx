f5d1bfcadb1086c7e6bea53491a829f155677740
nicarojasduocuc <nica.rojas@duocuc.cl>
2026-05-20 11:43:15 -0400

    cambio 1

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -20,7 +20,7 @@ export const PruebaCards = () => {
           }}
         />
         <CardComponent
-          title="Revisar Ordenes de despacho 🚚"
+          title="nicolas rojas sariego"
           description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
           buttonText="Consultar"
           onClick={() => {
